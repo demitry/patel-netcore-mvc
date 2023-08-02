029ef5081cd6f950186f059d1c0ca39202fc37e8
demitry <dpoluektov@gmail.com>
2023-08-02 18:24:02 +0300

    Client Side Validation [43]

--- Bulky/BulkyWeb/Controllers/CategoryController.cs
+++ Bulky/BulkyWeb/Controllers/CategoryController.cs
@@ -27,10 +27,10 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            //if (obj.Name == obj.DisplayOrder.ToString())
-            //{
-            //    ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name");
-            //}
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name");
+            }
 
             //if (obj.Name != null && obj.Name.ToLower() == "test")
             //{
