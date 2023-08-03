3603b7b0069cd1d1f61f8c9a66cc607b26c95515
demitry <dpoluektov@gmail.com>
2023-08-03 13:18:43 +0300

    Get Category Details to Edit [45]

--- Bulky/BulkyWeb/Controllers/CategoryController.cs
+++ Bulky/BulkyWeb/Controllers/CategoryController.cs
@@ -46,5 +46,42 @@ namespace BulkyWeb.Controllers
 
             return View();
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //var category = _db.Categories.Find(id); // id is primary key
+            var category = _db.Categories.FirstOrDefault(c => c.Id == id); // works even is id is not primary
+            //var category = _db.Categories.Where(category => category.Id == id).FirstOrDefault();
+            if (category == null)
+            {
+                return NotFound();
+            }
+            
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
     }
 }
