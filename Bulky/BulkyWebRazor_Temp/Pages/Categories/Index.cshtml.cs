956ae9d0fdc0e01d9f283a635133f657075d9ab3
demitry <dpoluektov@gmail.com>
2023-08-07 14:34:33 +0300

    Edit Category [60]

--- Bulky/BulkyWebRazor_Temp/Pages/Categories/Index.cshtml.cs
+++ Bulky/BulkyWebRazor_Temp/Pages/Categories/Index.cshtml.cs
@@ -1,6 +1,5 @@
 using BulkyWebRazor_Temp.Data;
 using BulkyWebRazor_Temp.Models;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BulkyWebRazor_Temp.Pages.Categories
